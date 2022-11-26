b8239d32a5d1f4a5696e4ddd8c3ec514f80ffd7e
rockwill <will.choi.kim@gmail.com>
2022-11-26 17:50:05 -0600

    OOPed the robo-arm, started working on input controls.

--- game/script.js
+++ game/script.js
@@ -1,9 +1,30 @@
+var velocity = [0, 0]
 function keydown(key) {
+    if (key == "w") {
+        velocity[1] = -1
+    } if (key == "s") {
+        velocity[1] = 1
+    }
 
+    if (key == "a") {
+        velocity[0] = -1
+    } if (key == "d") {
+        velocity[0] = 1
+    }
 }
 
 function keyup(key) {
+    if (key == "w" && velocity[1] == -1) {
+        velocity[1] = 0
+    } if (key == "s" && velocity[1] == 1) {
+        velocity[1] = 0
+    }
 
+    if (key == "a" && velocity[0] == -1) {
+        velocity[0] = 0
+    } if (key == "d" && velocity[0] == 1) {
+        velocity[0] = 0
+    }
 }
 
 function normalize(xy) {
@@ -16,101 +37,151 @@ function click(position) {
 
 }
 
+const gamepads = {};
+
+function gamepadHandler(event, connecting) {
+  const gamepad = event.gamepad;
+  // Note:
+  // gamepad === navigator.getGamepads()[gamepad.index]
+
+  if (connecting) {
+    gamepads[gamepad.index] = gamepad;
+  } else {
+    delete gamepads[gamepad.index];
+  }
+}
+
+window.addEventListener("gamepadconnected", (e) => { gamepadHandler(e, true); }, false);
+window.addEventListener("gamepaddisconnected", (e) => { gamepadHandler(e, false); }, false);s
+
+// var beingDragged = false
+class Robo {
+    constructor(a, b, c, d, r, q, direction, color, accent) {
+        this.a = a
+        this.b = b
+        this.c = c
+        this.d = d
+        this.r = r
+        this.q = q
+        this.i0 = {x:(r<q ? a:c) + (r<q ? r : q)*Math.cos(0*Math.PI/180), y:(r<q ? b:d) + (r<q ? r:q)*Math.sin(0*Math.PI/180)}
+        this.flip = direction
+        this.color = color
+        this.accent = accent
+        this.closest = [0, 0]
+        // this.currentlyDragging = false
+    }
+
+    draw() {
+        ctx.fillStyle = "white"
+        ctx.beginPath()
+        ctx.arc(this.a, this.b, hold, 0, 2*Math.PI)
+        ctx.closePath()
+        ctx.fill()
+        ctx.beginPath()
+        ctx.arc(this.c, this.d, hold, 0, 2*Math.PI)
+        ctx.closePath()
+        ctx.fill()
+
+        this.closest = [Math.abs(distance(this.i0,{x:this.a, y:this.b}) - this.r) + Math.abs(distance(this.i0,{x:this.c, y:this.d}) - this.q), this.i0, 0]
+        let m = (this.d-this.b)/(this.c-this.a)
+        let yint = this.b-m*this.a
+        for (let i=0;i<360;i+=360/accuracy) {
+            ctx.beginPath()
+            let center = {
+                x:(this.r<this.q ? this.a:this.c) + (this.r<this.q ? this.r : this.q)*Math.cos(i*Math.PI/180), 
+                y:(this.r<this.q ? this.b:this.d) + (this.r<this.q ? this.r:this.q)*Math.sin(i*Math.PI/180)
+            }
+            if (this.flip*(this.a < this.c ? 1 : -1)*center.y < this.flip*(this.a < this.c ? 1 : -1)*m*center.x + this.flip*(this.a < this.c ? 1 : -1)*yint) {
+                if (Math.abs(distance(center, {x:this.a, y:this.b}) - this.r) + Math.abs(distance(center, {x:this.c, y:this.d}) - this.q) < this.closest[0]) {
+                    this.closest = [Math.abs(distance(center, {x:this.a, y:this.b}) - this.r) + Math.abs(distance(center, {x:this.c, y:this.d}) - this.q), center, i]
+                }
+            }
+        }
+
+        ctx.lineWidth = 120
+        ctx.strokeStyle = this.color
+        ctx.beginPath()
+        ctx.moveTo(this.a, this.b)
+        ctx.lineTo(this.closest[1].x, this.closest[1].y)
+        ctx.closePath()
+        ctx.stroke()
+        ctx.beginPath()
+        ctx.moveTo(this.closest[1].x, this.closest[1].y)
+        ctx.lineTo(this.c, this.d)
+        ctx.moveTo(this.a, this.b)
+        ctx.closePath()
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.arc(this.closest[1].x, this.closest[1].y, 100, 0, 2*Math.PI)
+        ctx.closePath()
+        ctx.fillStyle = this.accent
+        ctx.fill()
+
+        ctx.fillStyle = this.accent
+        ctx.fillRect(this.a-100, this.b-50, 200, 100)
+
+        // if (distance({x:this.c,y:this.d}, mousePosition) < hold && !beingDragged) {
+        //     this.currentlyDragging = true
+        //     beingDragged = true
+        // }
+        // if (!mouseDown) {
+        //     this.currentlyDragging = false
+        //     beingDragged = false
+        // }
+        // if (this.currentlyDragging == true) {
+        //     // theta1 = Math.atan2((this.b-closest[1].y), -(this.a-closest[1].x))
+        //     // theta2 = Math.atan2(closest[1].y - this.d, -closest[1].x + this.c) + Math.PI - theta1
+        //     // theta1 *= 180/Math.PI
+        //     // theta2 *= 180/Math.PI
+        //     // console.log(theta2)
+        //     // if (theta1 > interval1[0] && theta1 < interval1[1] && theta2 > interval2[0] && theta2 < interval2[1]) {
+                
+                    
+                
+        //     // }
+        // }
+    }
+
+    move(x, y) {
+        if (distance({x:this.a, y:this.b}, {x:this.c + x, y:this.d + y}) > this.r + this.q) { 
+            let theta = Math.atan2(this.d + y - this.b, this.c + x - this.a)
+            this.c = this.a + (this.r + this.q)*Math.cos(theta)
+            this.d = this.b + (this.r + this.q)*Math.sin(theta)
+        } else {
+            this.c = this.c + x
+            this.d = this.d + y
+        }
+    }
+}
+
 var shaking = false
 var shakeFactor = 10
-var a = 125
-var b = 800
-var c = 600
-var d = 300
-var r = 400
-var q = 400
+var arms = [
+    new Robo(125, 800, 600, 300, 400, 400, 1, "yellow", "orange"),
+    new Robo(1411, 800, 936, 300, 400, 400, -1, "lightblue", "teal"),
+]
 var accuracy = 360
 const hold = 25
-var currentlyDragging = false
-const i0 = {x:(r<q ? a:c) + (r<q ? r : q)*Math.cos(0*Math.PI/180), y:(r<q ? b:d) + (r<q ? r:q)*Math.sin(0*Math.PI/180)}
 var lastAngle = 0
 var interval1 = [60, 120]
 var interval2 = [85, 180]
+var speed = 10
 function runtime() {
     ctx.save()
     if (shaking) {
         ctx.translate(Math.random() < 0.5 ? -Math.random()*shakeFactor : Math.random()*shakeFactor, Math.random() < 0.5 ? -Math.random()*shakeFactor : Math.random()*shakeFactor)
     }
 
-    ctx.fillStyle = "white"
+    ctx.fillStyle = "rgb(50, 50, 50)"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    ctx.fillStyle = "black"
-    ctx.beginPath()
-    ctx.arc(a, b, hold, 0, 2*Math.PI)
-    ctx.closePath()
-    ctx.fill()
-    ctx.beginPath()
-    ctx.arc(c, d, hold, 0, 2*Math.PI)
-    ctx.closePath()
-    ctx.fill()
-
-    closest = [Math.abs(distance(i0,{x:a, y:b}) - r) + Math.abs(distance(i0,{x:c, y:d}) - q), i0, 0]
-    m = (d-b)/(c-a)
-    yint = b-m*a
-    for (i=0;i<360;i+=360/accuracy) {
-        ctx.beginPath()
-        center = {x:(r<q ? a:c) + (r<q ? r : q)*Math.cos(i*Math.PI/180), y:(r<q ? b:d) + (r<q ? r:q)*Math.sin(i*Math.PI/180)}
-        if ((a < c ? 1 : -1)*center.y < (a < c ? 1 : -1)*m*center.x + (a < c ? 1 : -1)*yint) {
-            if (Math.abs(distance(center, {x:a, y:b}) - r) + Math.abs(distance(center, {x:c, y:d}) - q) < closest[0]) {
-                closest = [Math.abs(distance(center, {x:a, y:b}) - r) + Math.abs(distance(center, {x:c, y:d}) - q), center, i]
-            }
-        }
-    }
-
-    ctx.lineWidth = 120
-    ctx.strokeStyle = "rgb(250, 200, 0)"
-    ctx.beginPath()
-    ctx.moveTo(a, b)
-    ctx.lineTo(closest[1].x, closest[1].y)
-    ctx.closePath()
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.moveTo(closest[1].x, closest[1].y)
-    ctx.lineTo(c, d)
-    ctx.moveTo(a, b)
-    ctx.closePath()
-    ctx.stroke()
-
-    ctx.beginPath()
-    ctx.arc(closest[1].x, closest[1].y, 100, 0, 2*Math.PI)
-    ctx.closePath()
-    ctx.fillStyle = "orange"
-    ctx.fill()
-
-    ctx.fillStyle = "rgb(250, 100, 0)"
-    ctx.fillRect(25, 750, 200, 100)
+    arms[0].move(normalize(velocity)[0]*speed, normalize(velocity)[1]*speed)
+    arms.forEach((arm) => {
+        arm.draw()
+    })
     ctx.fillStyle = "gray"
     ctx.fillRect(0, 850, canvas.width, 200)
-
-    if (distance({x:c,y:d}, mousePosition) < hold) {
-        currentlyDragging = true
-    }
-    if (!mouseDown) {
-        currentlyDragging = false
-    }
-    if (currentlyDragging == true) {
-        theta1 = Math.atan2((b-closest[1].y), -(a-closest[1].x))
-        theta2 = Math.atan2(closest[1].y - d, -closest[1].x + c) + Math.PI - theta1
-        theta1 *= 180/Math.PI
-        theta2 *= 180/Math.PI
-        console.log(theta2)
-        // if (theta1 > interval1[0] && theta1 < interval1[1] && theta2 > interval2[0] && theta2 < interval2[1]) {
-            if (distance({x:a, y:b}, mousePosition) > r + q) {
-                theta = Math.atan2(-(b-mousePosition.y), -(a-mousePosition.x))
-                c = a + (r + q)*Math.cos(theta)
-                d = b + (r + q)*Math.sin(theta)
-            } else {
-                c = mousePosition.x
-                d = mousePosition.y
-            }
-        // }
-    }
 
     ctx.restore()
 }
